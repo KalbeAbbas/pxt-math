50b034e6998d30c8e84b4439eaca16b1e9c63f5c
KalbeAbbas <kalbeabbas142@gmail.com>
2020-05-11 10:09:45 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
-namespace Math
-{
-    //% blockId="sin_func"
-    //% block="sin %u"
-    export function sine(u: number)
-    {
-        return u
+namespace Math {
+
+    //% blockId="truncDec"
+    //% block="Truncate number %num to %dec decimal places"
+    export function truncate_decimal(num: number, dec: number):number {
+        let floor_num = Math.floor(num)
+        let str_num = num.toString().substr(0, floor_num.toString().length + 1 + dec)
+        let final_num = parseFloat(str_num)
+
+        return final_num
+
     }
 }
