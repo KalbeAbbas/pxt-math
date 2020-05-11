77b26b583b6d9db6ce369eea636465beb6116e67
KalbeAbbas <kalbeabbas142@gmail.com>
2020-05-11 10:16:43 +0500

    Updates.

--- main.ts
+++ main.ts
@@ -2,12 +2,13 @@ namespace Math {
 
     //% blockId="truncDec"
     //% block="Truncate number %num to %dec decimal places"
-    export function truncate_decimal(num: number, dec: number):number {
+    export function truncate_decimal(num: number, dec: number):string {
         let floor_num = Math.floor(num)
         let str_num = num.toString().substr(0, floor_num.toString().length + 1 + dec)
-        let final_num = parseFloat(str_num)
 
-        return final_num
+
+        return str_num
 
     }
+
 }
